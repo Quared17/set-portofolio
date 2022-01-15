c50dd81651153d625a2e817c718f8b9247710962
adi yuwono <adiyuwono680@gmail.com>
2022-01-15 20:35:16 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="style.css" rel="stylesheet" type="text/css">
+	<link href="responsive.css" rel="stylesheet" type="text/css">
+	<link href="fontAwesome/css/all.min.css" rel="stylesheet">
+	<script src="jquery/jquery.min.js" type="text/javascript"></script>
+	<title> Hello World </title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+	<div id="container-navbar">
+		<h2 id="logo">
+			Quared.id
+		</h2>
+
+		<div class="container-nav-link">
+			<a class="nav-link" href="#about-section"> About </a>
+			<a class="nav-link" href="#skill-section"> My Skill </a>
+			<a class="nav-link" href="#ortofolio-section"> My Portofolio </a>
+			<a class="nav-link active" href="#contact-section"> Contact Me </a>
+		</div>
+	</div>
+
+	<div class="container-content" id="about-section"> </div>
+
+	<div class="container-content" id="skill-section"> 
+		<h1 class="container-title-section">
+			My Skill
+		</h1>
+
+		<div class="container-grid">
+			<div class="list-skill-item"> 
+				<i class="fab fa-html5 item-html"></i>
+
+				<h3> HTML 5 </h3>
+			</div>
+
+			<div class="list-skill-item">
+				<i class="fab fa-css3-alt item-css"></i>
+
+				<h3> CSS 3 </h3>
+			</div>
+
+			<div class="list-skill-item">
+				<i class="fab fa-php item-php"></i>
+
+				<h3> PHP </h3>
+			</div>
+
+			<div class="list-skill-item">
+				<i class="fab fa-js-square item-javascript"></i>
+
+				<h3> Javascript </h3>
+			</div>
+
+			<div class="list-skill-item">
+				<i class="fab fa-github-alt item-github"></i>
+
+				<h3> Git | Github </h3>
+			</div>
+
+			<div class="list-skill-item">
+				<i class="fab fa-bootstrap item-boostrap"></i>
+
+				<h3> Boostrap </h3>
+			</div>
+
+			<div class="list-skill-item">
+				<i class="fab fa-laravel item-laravel"></i>
+
+				<h3> Laravel </h3>
+			</div>
+
+			<div class="list-skill-item">
+				<i class="fab fa-wordpress item-wordpress"></i>
+
+				<h3> Wordpress </h3>
+			</div>
+
+			<div class="list-skill-item">
+				<i class="fab fa-figma item-figma"></i>
+
+				<h3> Figma </h3>
+			</div>
+
+		</div>
+
+		<!-- <div class="container-grid">
+			<div class="list-skill-item">
+				<i class="fab fa-html5 item-html"></i>
+
+				<h3> HTML 5 </h3>
+			</div>
+
+			<div class="list-skill-item">
+				<i class="fab fa-css3-alt item-css"></i>
+
+				<h3> CSS 3 </h3>
+			</div>
+
+			<div class="list-skill-item">
+				<div class="container-logo-skill">
+					<img src="Image/grid.png">
+				</div>
+
+				<h3> Grid | Flexbox </h3>
+			</div>
+
+			<div class="list-skill-item">
+				<i class="fab fa-bootstrap item-boostrap"></i>
+
+				<h3> Boostrap </h3>
+			</div>
+
+			<div class="list-skill-item">
+
+				<h3> UI | UX </h3>
+			</div>
+
+			<div class="list-skill-item">
+				<i class="fab fa-js-square item-javascript"></i>
+
+				<h3> Javascript </h3>
+			</div>
+
+			<div class="list-skill-item">
+				
+
+				<h3> Jquery </h3>
+			</div>
+
+			<div class="list-skill-item">
+				<i class="fab fa-php item-php"></i>
+
+				<h3> PHP </h3>
+			</div>
+
+			<div class="list-skill-item">
+
+				<h3> OOP PHP </h3>
+			</div>
+
+			<div class="list-skill-item">
+
+				<h3> MVC PHP </h3>
+			</div>
+
+			<div class="list-skill-item">
+				<i class="fab fa-laravel item-laravel"></i>
+
+				<h3> Laravel </h3>
+			</div>
+
+			<div class="list-skill-item">
+				<div class="container-logo-skill">
+					<img src="Image/api.png">
+				</div>
+
+				<h3> API </h3>
+			</div>
+
+			<div class="list-skill-item">
+				<div class="container-logo-skill">
+					<img src="Image/mysql.png">
+				</div>
+
+				<h3> MySQL </h3>
+			</div>
+
+			<div class="list-skill-item">
+				<i class="fas fa-search"></i>
+
+				<h3> SEO </h3>
+			</div>
+
+			<div class="list-skill-item">
+				<i class="fab fa-github-alt item-github"></i>
+
+				<h3> Git | Github </h3>
+			</div>
+		</div> -->
+
+	</div>
+
+	<div class="container-content" id="portofolio-section"> </div>
+	<div class="container-content" id="contact-section"> </div>
+
+	<script type="text/javascript">
+		// $(document).ready(function(){
+		// 	$('.nav-link').click(function(){
+		// 		$('.nav-link').siblings('active');
+		// 	})
+		// });
+	</script>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
